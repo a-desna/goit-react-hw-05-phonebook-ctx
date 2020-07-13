5b05be93b6b5dfa03768faef9628f556aca1ab69
a-desna <a.sychuk@ukr.net>
2020-07-13 12:12:59 +0300

    Updates

---
+++ precache-manifest.102fe660964eb5fc4e677edac5f05702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ac77ac175ce52a371807bf153603c0",
+    "url": "/goit-react-hw-03-phonebook-ctx/index.html"
+  },
+  {
+    "revision": "07b081c2c9cdc58dac52",
+    "url": "/goit-react-hw-03-phonebook-ctx/static/css/main.4cc7e530.chunk.css"
+  },
+  {
+    "revision": "01a6afa1cc9064fca6ca",
+    "url": "/goit-react-hw-03-phonebook-ctx/static/js/2.017aea50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook-ctx/static/js/2.017aea50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b081c2c9cdc58dac52",
+    "url": "/goit-react-hw-03-phonebook-ctx/static/js/main.48ce5cd1.chunk.js"
+  },
+  {
+    "revision": "7de9f10f59dc0b02f815",
+    "url": "/goit-react-hw-03-phonebook-ctx/static/js/runtime-main.1e4df644.js"
+  }
+]);
